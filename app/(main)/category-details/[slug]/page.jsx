95888bd779ad1289eccc902f127b3f9bb1daf062
Imran <57107484+imrageco@users.noreply.github.com>
2025-02-20 13:55:58 +0500

    fix category error

--- app/(main)/category-details/[slug]/page.jsx
+++ app/(main)/category-details/[slug]/page.jsx
@@ -1,19 +1,23 @@
-import Header from '@/app/components/Header'
-import React from 'react'
+import Header from '@/app/components/Header';
+import React from 'react';
 
-export const metadata = {
-    title: params.slug.replace('-', ' ') + 'E-Learn'
+// Function to generate dynamic metadata
+export async function generateMetadata({ params }) {
+    return {
+        title: params.slug.replace('-', ' ') + ' E-Learn',
+    };
 }
 
 const CategoryDetails = ({ params }) => {
     return (
-        <Header>
-            <h1 className="mb-4 text-2xl font-extrabold tracking-tight leading-none
+        <div>
+            <Header />
+            <h1 className="mb-4 text-2xl font-extrabold tracking-tight leading-none 
             text-gray-900 md:text-3xl lg:text-3xl dark:text-white">
                 {params.slug.replace('-', ' ').toUpperCase()}
             </h1>
-        </Header>
-    )
-}
+        </div>
+    );
+};
 
-export default CategoryDetails
+export default CategoryDetails;
